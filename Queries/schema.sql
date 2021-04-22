990d2e18b5c697063ed45561b8e79042ddd89d11
unknown <tessac1995@gmail.com>
2021-04-22 15:43:46 -0500

    final merges

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -52,3 +52,27 @@ CREATE TABLE Titles (
 );
 
 --DROP TABLE employees CASCADE;
+
+-- Joining departments and dept_manager tables
+SELECT departments.dept_name,
+     dept_manager.emp_no,
+     dept_manager.from_date,
+     dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+	 FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no;
